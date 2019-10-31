924e69b09080d99f50afa1bda2fd30a44997af61
Minh Phan <minhphan156@gmail.com>
2019-10-31 10:11:55 -0700

    added appbar, social card

---
+++ src/components/SocialPage.js
@@ -0,0 +1,125 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+import Button from "@material-ui/core/Button";
+
+import MenuIcon from "@material-ui/icons/Menu";
+
+const styles = theme => ({
+  card: {
+    maxWidth: 9000
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: red[500]
+  },
+  root: {
+    flexGrow: 1,
+    backgroundColor: "white"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+});
+
+class SocialPage extends Component {
+  render() {
+    const classes = this.props.classes;
+    return (
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12}>
+          <div className={classes.root}>
+            <AppBar position="static" style={{ backgroundColor: "white" }}>
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  className={classes.menuButton}
+                  color="black"
+                  aria-label="menu"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Button color="inherit" color="black">
+                  Login
+                </Button>
+              </Toolbar>
+            </AppBar>
+          </div>
+        </Grid>
+        <Grid item xs={6}>
+          <Card className={classes.card}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  R
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title="Shrimp and Chorizo Paella"
+              subheader="September 14, 2016"
+            />
+            <CardMedia
+              className={classes.media}
+              image="https://images.tienda.com/is/image/LaTienda/mixed-seafood-paella?&wid=1136"
+              title="Paella dish"
+            />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                This impressive paella is a perfect party dish and a fun meal to
+                cook together with your guests. Add 1 cup of frozen peas along
+                with the mussels, if you like.
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default withStyles(styles)(SocialPage);
