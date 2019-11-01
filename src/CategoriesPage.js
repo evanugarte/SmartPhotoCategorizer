c0ca2d2db4d300eae53c4bddb5ae12438e60a7ea
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2019-11-01 15:41:42 -0700

    Added Routing + Categories and Image Component (#6)
    See images below

---
+++ src/CategoriesPage.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Card, CardImg, CardBody,
+  CardTitle, CardSubtitle,
+  Container, Button
+} from "reactstrap";
+import img from "./black.png";
+import "./App.css";
+
+function Employees(props) {
+
+  const categories = [
+    { name: "nature", count: 10 },
+    { name: "restaurant", count: 7 },
+    { name: "cars", count: 19 }
+  ];
+
+  function renderPersonEntries() {
+    return (
+      categories.map((category, index) => {
+        return (
+          <Card key={index} style={{ width: "70%" }}>
+            <CardImg top style={{ width: "50%" }} src={img} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>{category.name}</CardTitle>
+              <CardSubtitle>{category.count} photos</CardSubtitle>
+              <Button onClick={() =>
+                props.history.push(`/category/${category.name}`)}>
+                Visit</Button>
+            </CardBody>
+          </Card>
+        );
+      })
+    );
+    // }
+  }
+
+  return (
+    <Container style={{ textAlign: "left" }} className="Employees">
+      {renderPersonEntries()}
+    </Container>
+  );
+}
+
+export default Employees;
