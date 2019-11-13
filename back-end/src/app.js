722cda02def370c2c8cf76009a655b4df78903ed
goldfishyyy <yanyan.yang@sjsu.edu>
2019-11-13 11:04:45 -0800

    modified Narvar

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -9,6 +9,35 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+//----------------------
+// const CognitoExpress=require("cognito-express");
+// const authenticatedRoute=express.Router();
+
+// const cognitoExpress=new CognitoExpress({
+//   region: "us-west-2",
+//   cognitoUserPoolID: "us-west-2_wf2PodP2N",
+//   tokenUse: "access",
+//   tokenExpriration: 3600000
+// });
+// //authenticate All API under authenticatedROute router
+// authenticatedRoute.use(function(req,res,next){
+//   let accessTokenFromClient=req.headers.accesstoken;
+//   if(!accessTokenFromClient) 
+//     return res.status(401).send("Access Token missing from header");
+//   cognitoExpress.validate(accessTokenFromClient,function(err,response){
+//     if(err)
+//       return res.status(401).send(err);
+//     else{
+//       res.locals.user=response;
+//       next();
+//     }
+//   });
+// });
+// //define routes that need authentication check
+// authenticatedRoute.get("/api",function(req,res,next){
+//   res.send("200");
+// });
+
 
 // view engine setup
 app.set("views", path.join(__dirname, "../views"));
@@ -18,6 +47,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+
+
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
