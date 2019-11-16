ee12f3757de8b1077a978c4beaaf2162ae017956
LetTheBassKick <royzhang42@gmail.com>
2019-11-15 17:20:09 -0800

    replaced request.body with request.query for get requests

--- back-end/src/routes/api.js
+++ back-end/src/routes/api.js
@@ -103,7 +103,7 @@ const getPhotoSocial = (request, response) => {
   (async () => {
     var statusCode = 400;
     var message = "User does not exist";
-    const reqEmail = request.body.email;
+    const reqEmail = request.query.email;
     
     // For now this will be in the headers. Modify later maybe.
     if (reqEmail) {
@@ -173,7 +173,7 @@ const getPhotoByTag = (request, response) => {
   (async () => {
     var statusCode = 400;
     var message = "Empty";
-    const reqTag = request.body.tag;
+    const reqTag = request.query.tag;
     if (reqTag) {
       try {
         var queryParams = {
