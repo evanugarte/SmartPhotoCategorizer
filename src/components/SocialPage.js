8a8849b25dc143a38acc027d13f219241f8c5db3
Minh Phan <minhphan156@gmail.com>
2019-11-16 21:27:57 -0800

    remove space

--- src/components/SocialPage.js
+++ src/components/SocialPage.js
@@ -55,7 +55,6 @@ class SocialPage extends Component {
   componentWillMount(){
     //TODO: need to get this userid, email from backend first
     const {userInfo} = this.props.user;
-    
     this.props.getPicsSocialAction(userInfo);
     this.props.getProfileFileAction(userInfo);
   }
